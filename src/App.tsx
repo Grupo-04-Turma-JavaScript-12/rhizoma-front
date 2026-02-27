46a1416c849ca3a22f97eeb6cee4e53e9197b9f5
Bruna Melo <bmelo.silva25@gmail.com>
2026-02-27 08:41:26 -0300

    Sobre o Produto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import SobreoProduto from "./sobreoproduto/SobreProduto";
+
 
 function App() {
   return (
     <>
+    <SobreoProduto/>
+    {/* <BrowserRouter> */}
       <div>
-        <h1>Olá</h1>
+          {/* <Routes> */}
+        {/* <Route path="/sobreoproduto" element={<SobreoProduto/>} /> */}
+        {/* </Routes> */}
       </div>
+      {/* </BrowserRouter> */}
     </>
   );
 }
